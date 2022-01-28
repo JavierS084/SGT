8345dbb2952295d8739ab180aa74a1e2624e43f7
JavierS084 <javier.sanchezb084@gmail.com>
2022-01-28 10:16:53 -0300

    V2.7

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,9 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'newpassword',
-        database: 'db_sgt'
+        host: '22b354d1@us-cdbr-east-05.cleardb.net',
+        user: 'b98ed115a3df02',
+        password: 'b98ed115a3df02',
+        database: 'heroku_ca17ab47c9adebf'
+        
     }
 };
